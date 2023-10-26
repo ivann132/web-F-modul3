7113274a33e16d9f678f5ed96666c8b72dbbd1d5
ivann132 <ivanprayogaadi@gmail.com>
2023-10-26 09:23:52 +0700

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../Components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <p>Halaman Home</p>
+    </div>
+  );
+};
+
+export default Home;
